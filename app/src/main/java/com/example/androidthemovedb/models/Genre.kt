6f6f169e9e57fdfeb6fcc84b06393c711125da2c
Linad <merzlikindaniil97@gmail.com>
2024-11-30 18:21:21 +0300

    add model themove

---
+++ app/src/main/java/com/example/androidthemovedb/models/Genre.kt
@@ -0,0 +1,6 @@
+package com.example.androidthemovedb.models
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/example/androidthemovedb/models/SpokenLanguage.kt
@@ -0,0 +1,7 @@
+package com.example.androidthemovedb.models
+
+data class SpokenLanguage(
+    val english_name: String,
+    val iso_639_1: String,
+    val name: String
+)
